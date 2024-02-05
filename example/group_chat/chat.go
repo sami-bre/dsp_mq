bff5f517381e8fe84e5273c9a73b5311bb0e06f4
Samuel <samibree121@gmail.com>
2024-02-05 22:55:19 +0300

    updating the simple consumer and producer to publish and consume multiple messages

--- example/group_chat/chat.go
+++ example/group_chat/chat.go
@@ -11,6 +11,7 @@ import (
 var addr = flag.String("addr", "127.0.0.1:11181", "dsp_mq listen address")
 var queue = flag.String("queue", "test_queue", "queue want to bind")
 
+
 func main() {
 	flag.Parse()
 
@@ -24,8 +25,15 @@ func main() {
 	defer c.Close()
 
 	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Enter your name: ")
+	var name string
+	fmt.Scanln(&name)
+
+	go listener()
+
 	for {
-		fmt.Print("Enter a message (or 'exit' to quit): ")
+		// fmt.Print("")
 		scanner.Scan()
 		msg := scanner.Text()
 
@@ -33,9 +41,38 @@ func main() {
 			break
 		}
 
-		_, err = c.PublishFanout(*queue, []byte(msg))
+		_, err = c.PublishFanout(*queue, []byte(name+": "+msg))
 		if err != nil {
 			panic(err)
 		}
+	}
+}
+
+func listener() {
+	flag.Parse()
+
+	cfg := client.NewDefaultConfig()
+	cfg.BrokerAddr = *addr
+
+	c, err := client.NewClientWithConfig(cfg)
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+
+	var conn *client.Conn
+	conn, err = c.Get()
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	var ch *client.Channel
+	ch, err = conn.Bind(*queue, "", true)
+
+	for {
+		msg := ch.GetMsg()
+		fmt.Println(string(msg))
 	}
 }
